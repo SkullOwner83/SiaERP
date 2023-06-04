edeeeaefc84daac61fac7de9131aef46f2dc9aa0
SkullOwner83 <skullowner83@gmail.com>
2023-06-04 17:58:26 -0600

    Firs commit, window setting

---
+++ SiaERP/Models/Customer.cs
@@ -0,0 +1,99 @@
+﻿namespace Models
+{
+    public class Customer
+    {
+		//Customer fields
+        private int id;
+        private int type;
+        private string name;
+        private string rfc;
+        private string phonenumber;
+        private string email;
+        private string adress;
+        private string city;
+        private string state;
+        private string country;
+        private string postalcode;
+        private string taxtregime;
+		private string registerdate;
+
+		//Customer Properties
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		public int Type
+		{
+			get { return type; }
+			set { type = value; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public string RFC
+		{
+			get { return rfc; }
+			set { rfc = value; }
+		}
+
+		public string PhoneNumber
+		{
+			get { return phonenumber; }
+			set { phonenumber = value; }
+		}
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+		public string Adress
+		{
+			get { return adress; }
+			set { adress = value; }
+		}
+
+		public string City
+		{
+			get { return city; }
+			set { city = value; }
+		}
+
+		public string State
+		{
+			get { return state; }
+			set { state = value; }
+		}
+
+		public string Country
+		{
+			get { return country; }
+			set { country = value; }
+		}
+
+		public string PostalCode
+		{
+			get { return postalcode; }
+			set { postalcode = value; }
+		}
+
+		public string TaxtRegime
+		{
+			get { return taxtregime; }
+			set { taxtregime = value; }
+		}
+
+		public string RegisterDate
+		{
+			get { return registerdate; }
+			set { registerdate = value; }
+		}
+    }
+}
---
+++ SiaERP/Views/Main.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace SiaERP.Views
+{
+	public partial class Main : Window
+	{
+		private GridLength MenuMinSize = new GridLength(64);
+		private GridLength MenuMaxSize = new GridLength(256);
+
+		//Constructor Method
+		public Main()
+		{
+			InitializeComponent();
+			this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+			this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+		}
+
+		//Import windows drag native library
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+		//Window movement and behavior control
+		private void MtdDragWindow(object sender, MouseButtonEventArgs e)
+		{
+			//Move window when clicking on header
+			if (e.ClickCount == 1)
+			{
+				if (e.LeftButton == MouseButtonState.Pressed)
+				{
+					WindowInteropHelper helper = new WindowInteropHelper(this);
+					SendMessage(helper.Handle, 161, 2, 0);
+				}
+			}
+			//Maximized or restore window when double clicking on header
+			else if (e.ClickCount == 2)
+			{
+				MtdWindowMaximize();
+			}
+		}
+
+		//Button cliked function controls
+		public void MtdButtonClicked(object sender, RoutedEventArgs e)
+		{
+			switch(((Button)sender).Name)
+			{
+				//Window behavior buttons control
+				case "BtnWindowMinimize": WindowState = WindowState.Minimized; break;
+				case "BtnWindowMaximize": MtdWindowMaximize(); break;
+				case "BtnWindowClose": Application.Current.Shutdown(); break;
+
+				case "BtnExpandPanel":
+					if (PnlMenu.Width == MenuMinSize)
+						PnlMenu.Width = MenuMaxSize;
+					else
+						PnlMenu.Width = MenuMinSize;
+				break;
+			}
+		}
+
+		//Interchanged between maximize and normal window state
+		public void MtdWindowMaximize()
+		{
+			if (WindowState == WindowState.Maximized)
+			{
+				WindowState = WindowState.Normal;
+			}
+			else if (WindowState == WindowState.Normal)
+			{
+				WindowState = WindowState.Maximized;
+			}
+		}
+	}
+}
